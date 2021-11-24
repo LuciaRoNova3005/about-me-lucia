53c4ab95834acc343bb67082f8a08bb933a2abfe
LuciaRoNova3005 <lucia.r.nova@gmail.com>
2021-11-24 12:45:45 +0100

    add linkcontact in cover

--- src/components/Cover.js
+++ src/components/Cover.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Nav from "./Nav.js";
 import imagen from "../img/lucia-cv.jpg";
+import LinkContact from "./linkContact.js";
 
 const Cover = () => {
   return (
@@ -11,7 +12,8 @@ const Cover = () => {
         <div className="cover">
           <div className="cover__text">
             <h1 className="cover__text__title1">Lucía Rodríguez</h1>
-            <h2 className="cover__text__title">Junior Frontend Developer</h2>
+            <h2 className="cover__text__title">Front-end Developer</h2>
+            <LinkContact></LinkContact>
           </div>
           <img className="cover__img" src={imagen} alt=""></img>
         </div>
---
+++ src/components/linkContact.js
@@ -0,0 +1,26 @@
+function LinkContact() {
+  return (
+    <div className="linkContainer">
+      <a href="https://github.com/LuciaRoNova3005/" target="_blank">
+        <span>
+          <i className="fab fa-github"></i>
+        </span>
+      </a>
+      <a href="https://www.linkedin.com/in/luciarodrigueznova/" target="_blank">
+        <span>
+          <i className="fab fa-linkedin-in"></i>
+        </span>
+      </a>
+      <a
+        href="./CV-Lucia-Rodriguez-Nova-frontend-development.pdf"
+        target="_blank"
+      >
+        <span className="">
+          <i className="far fa-file-pdf"></i>
+        </span>
+      </a>
+    </div>
+  );
+}
+
+export default LinkContact;
