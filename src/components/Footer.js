9403beb75eb5e85bf8cd27f59c5957bd15f1d312
LuciaRoNova3005 <lucia.r.nova@gmail.com>
2021-11-25 19:50:03 +0100

    Add footer html and styles

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,25 +1,29 @@
 const Footer = () => {
   return (
     <footer id="contact" className="footer">
-      <div className="">
+      <h3 className="tittle3 footer__tittle3">
+        No dudes en contactar conmigo ¡hablemos!
+      </h3>
+      <div className="footer__link">
         <a
+          className="footer__link"
           href="mailto:lucia.r.nova@gmail.com/"
           target="_blank"
           rel="noreferrer"
         >
-          <span>
-            <i class="far fa-envelope"></i>
-          </span>
+          <i class="far fa-envelope"></i>
         </a>
         <a
+          className="footer__link"
           href="https://www.linkedin.com/in/luciarodrigueznova/"
           target="_blank"
           rel="noreferrer"
         >
-          <span>
-            <i className="fab fa-linkedin-in"></i>
-          </span>
+          <i className="fab fa-linkedin-in"></i>
         </a>
+      </div>
+      <div className="footer__copy">
+        <p className="">&copy; 2021 Lucía Rodríguez Nova</p>
       </div>
     </footer>
   );
