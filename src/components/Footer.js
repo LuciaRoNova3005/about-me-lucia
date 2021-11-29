c57a5d349468354dea8ac1b9eab4ba5fff227aa0
LuciaRoNova3005 <lucia.r.nova@gmail.com>
2021-11-29 11:54:09 +0100

    Add resposive footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,22 +3,24 @@ const Footer = () => {
     <footer id="contact" className="footer">
       <h3 className="title3">Contacto</h3>
 
-      <div className="footer__link">
+      <div className="footer__item">
         <a
-          className="footer__link"
+          className="footer__item__link"
           href="mailto:lucia.r.nova@gmail.com/"
           target="_blank"
           rel="noreferrer"
         >
           <i class="far fa-envelope"></i>
+          <p className="">Mail</p>
         </a>
         <a
-          className="footer__link"
+          className="footer__item__link"
           href="https://www.linkedin.com/in/luciarodrigueznova/"
           target="_blank"
           rel="noreferrer"
         >
           <i className="fab fa-linkedin-in"></i>
+          <p className="">Linkedin</p>
         </a>
       </div>
       <p className="footer__tittle3">
