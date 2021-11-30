49fc2c89d7718dace48febe27156666628618f60
LuciaRoNova3005 <lucia.r.nova@gmail.com>
2021-11-30 11:33:29 +0100

    Add </I18nextProvider> to appjs

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from "react-dom";
 import { HashRouter } from "react-router-dom";
 import "./stylesheets/App.scss";
 import App from "./components/App";
+import { I18nextProvider } from "react-i18next";
+import i18next from "i18next";
 
 ReactDOM.render(
   <React.StrictMode>
     <HashRouter>
-      <App />
+      <I18nextProvider i18n={i18next}>
+        <App />
+      </I18nextProvider>
     </HashRouter>
   </React.StrictMode>,
   document.getElementById("root")
