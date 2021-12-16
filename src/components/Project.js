5454bf51df9bcff3a3b9152971596828e3eda4bf
LuciaRoNova3005 <lucia.r.nova@gmail.com>
2021-12-16 11:21:21 +0100

    Add displa flex to icon projects

--- src/components/Project.js
+++ src/components/Project.js
@@ -15,16 +15,25 @@ function Project(props) {
           <h4 className="title4">{props.name}</h4>
           <p className="text">{props.description}</p>
           <p className="projects__card__infor__text__tools">{props.tools}</p>
-          <div className="projects__card__infor__text__link">
-            <a href={props.git} target="_blank" rel="noreferrer">
-              <i class="iconlink fab fa-github"></i>
-            </a>
-            <a href={props.web} target="_blank" rel="noreferrer">
+          {/* <div className="projects__card__infor__text__link"> */}
+          <a
+            className="projects__card__infor__text__link"
+            href={props.git}
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i class="projects__card__infor__text__link__iconlink fab fa-github"></i>
+            <p className="projects__card__infor__text__link__urlText">
+              Ir al repositorio
+            </p>
+          </a>
+
+          {/* <a href={props.web} target="_blank" rel="noreferrer">
               <i class="iconlink fas fa-laptop"></i>
-            </a>
-          </div>
+            </a> */}
         </div>
       </div>
+      {/* </div> */}
     </article>
   );
 }
